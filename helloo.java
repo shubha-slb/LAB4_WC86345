871aa72af8dc2daa4528f8cd33600f4d9ea4cb81
shubha-slb <shubhalaxmibiswal6@gmail.com>
2025-07-10 11:23:32 +0530

    Update helloo.java

--- helloo.java
+++ helloo.java
@@ -1,4 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, world_2021WA86298!");
+        System.out.println("Hello, world_2021WC86345!");
+        system.out.println("Hello world02");
     }
